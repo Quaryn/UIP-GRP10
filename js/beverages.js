97176e646498520ea3ee9a974142ce0cd35ff26e
Hullberg <johan.hullberg@gmail.com>
2017-02-21 11:33:59 +0100

    changed from 2darray to object array of Beverage()

--- js/beverages.js
+++ js/beverages.js
@@ -36,30 +36,33 @@
 // "producent" : "Reh Kendermann","leverantor" : "Hermansson & Co AB","argang" : "2013","provadargang" : "2013",
 // "alkoholhalt" : "8.5%","modul" : "","sortiment" : "FS","ekologisk" : "0","koscher" : "0"}]}
 
-function Beverage(name1, name2, sbl_price, pub_price, beer_id, count, price) {
-
-    this.name1 = name1; // Name of the beer
-    this.name2 = name2; // Sometimes two strings for name
-    this.sbl_price = sbl_price; // Choco: if Lars clarified that sbl_price can be ignored, why do we still keep it?
-    this.pub_price = pub_price; // Amount the customer pays
-    this.beer_id = beer_id;
-    this.count = count; /* Choco: does count controls the stock of the beer?
-    And we can create a threshold to issue an alarm when it goes low/crosses the predefined count?*/
-    this.price = price; // Choco: wholesale price the pub pays to the beer suppliers
-
+function Beverage(nr, artid, vnr, pname, price, volume, pgroup, container, seal, origin, originCountry, producer, distributor, alcohol) {
+    this.nr = nr;
+    this.artid = artid;
+    this.vnr = vnr;
+    this.pname = pname;
+    this.price = price;
+    this.volume = volume;
+    this.pgroup = pgroup;
+    this.container = container;
+    this.seal = seal;
+    this.origin = origin;
+    this.originCountry = originCountry;
+    this.producer = producer;
+    this.distributor = distributor;
+    this.alcohol = alcohol;
 
 }
 
 
-// To be used below
-// Choco: move close to the code where this function is used?
-function Create2DArray(rows) {
+// Don't know if we need it anymore
+/*function Create2DArray(rows) {
     var arr = [];
     for (var i=0;i<rows;i++) {
         arr[i] = [];
     }
     return arr;
-}
+}*/
 
 // Will return an array that where each item contains a beer_id. This is only used to use in another function to get
 // more detailed info about each beer.
@@ -68,6 +71,8 @@ If so, change the comment above to reflect this purpose.
 And it doesn't seem only used with the function getDetailedBeers ().
 When users load the menu page, it will return beers in the menu
 after checking the inventory, which will show available beers.*/
+// As it says in the comment above, this function is solely used to retrieve the beer_id's that exist. What you do
+// with this information can vary. So it shouldn't change.
 // Could add an option to only add if the inventory is higher than a certain amount
 // Choco: Agree with the threshold setting.
 function getBeers() {
@@ -85,24 +90,27 @@ function getBeers() {
 function getDetailedBeers() {
     var beers = getBeers(); // An array containing beer_id's
     // We want one row for each beer_id, no need to specify columns.
-    // detailedBeers[0][3] will access first row, fourth column
-    var detailedBeers = Create2DArray(beers.length);
+    var detailedBeers = [];
     for (var i = 0; i < beers.length; i++) {
         var response = JSON.parse(HTTPGetRequest("action=beer_data_get&beer_id=" + beers[i].toString()));
-        detailedBeers[i][0] = response["payload"][i]["nr"];
-        detailedBeers[i][1] = response["payload"][i]["artikelid"];
-        detailedBeers[i][2] = response["payload"][i]["varnummer"];
-        detailedBeers[i][3] = response["payload"][i]["namn"] + " " + response["payload"][i]["namn2"];
-        detailedBeers[i][4] = response["payload"][i]["prisinklmoms"];
-        detailedBeers[i][5] = response["payload"][i]["volymiml"];
-        detailedBeers[i][6] = response["payload"][i]["varugrupp"];
-        detailedBeers[i][7] = response["payload"][i]["forpackning"];
-        detailedBeers[i][8] = response["payload"][i]["forslutning"];
-        detailedBeers[i][9] = response["payload"][i]["ursprung"];
-        detailedBeers[i][10] = response["payload"][i]["ursprunglandnamn"];
-        detailedBeers[i][11] = response["payload"][i]["producent"];
-        detailedBeers[i][12] = response["payload"][i]["leverantor"];
-        detailedBeers[i][13] = response["payload"][i]["alkoholhalt"];
+
+        var nr = response["payload"][i]["nr"];
+        var artnr = response["payload"][i]["artikelid"];
+        var vnr = response["payload"][i]["varnummer"];
+        var pname = response["payload"][i]["namn"] + " " + response["payload"][i]["namn2"];
+        var price = response["payload"][i]["prisinklmoms"];
+        var volume = response["payload"][i]["volymiml"];
+        var pgroup = response["payload"][i]["varugrupp"];
+        var container = response["payload"][i]["forpackning"];
+        var seal = response["payload"][i]["forslutning"];
+        var origin = response["payload"][i]["ursprung"];
+        var originCountry = response["payload"][i]["ursprunglandnamn"];
+        var producer = response["payload"][i]["producent"];
+        var distributor = response["payload"][i]["leverantor"];
+        var alcohol = response["payload"][i]["alkoholhalt"];
+
+        var bev = new Beverage(nr,artnr,vnr,pname,price,volume,pgroup,container,seal,origin,originCountry,producer,distributor,alcohol);
+        detailedBeers.push(bev);
     }
     return detailedBeers;
 }
@@ -123,49 +131,40 @@ function sortBeverageByType () {
     var len = detailedBeers.length;
 
     // Not sure how to get a good length on all arrays. Maybe temporary arrays, then make new ones with correct length?
-    var lagers = Create2DArray(len);
-    var ales = Create2DArray(len);
-    var porterStout = Create2DArray(len);
-    var alcFree = Create2DArray(len);
-    var whiteWine = Create2DArray(len); // Choco: it can be commented out for future use
-    var redWine = Create2DArray(len); // Choco: it can be commented out for future use
-    var ciders = Create2DArray(len); // Choco: it can be commented out for future use
-    var otherBeverages = Create2DArray(len); // Choco: it can be commented out for future use
+    var lagers = [];
+    var ales = [];
+    var porterStout = [];
+    var alcFree = [];
+    var whiteWine = []; // Choco: it can be commented out for future use // Or not
+    var redWine = []; // Choco: it can be commented out for future use
+    var ciders = []; // Choco: it can be commented out for future use
+    var otherBeverages = []; // Choco: it can be commented out for future use
 
     for (var i = 0; i < len; i++) {
-        var type = detailedBeers[i][6]
-        var temp = 0;
+        var type = detailedBeers[i].pgroup;
         if (type == "Öl, Ljus lager") {
-            temp = lagers.length;
-            lagers[temp] = detailedBeers[i];
+            lagers.push(detailedBeers[i]);
         }
         else if (type == "Öl, Ale") {
-            temp = ales.length;
-            ales[temp] = detailedBeers[i]
+            ales.push(detailedBeers[i]);
         }
         else if (type == "Öl, Porter och Stout") {
-            temp = porterStout.length;
-            porterStout[temp] = detailedBeers[i];
+            porterStout.push(detailedBeers[i]);
         }
         else if (type.substring(0, 11) == "Alkoholfritt") {
-            temp = alcFree.length;
-            alcFree[temp] = detailedBeers[i];
+            alcFree.push(detailedBeers[i]);
         }
         else if (type.substring(0, 7) == "Vitt vin") {
-            temp = whiteWine.length;
-            whiteWine[temp] = detailedBeers[i];
+            whiteWine.push(detailedBeers[i]);
         }
         else if (type.substring(0, 7) == "Rött vin") {
-            temp = redWine.length;
-            redWhine[temp] = detailedBeers[i];
+            redWine.push(detailedBeers[i]);
         }
         else if (type.substring(0, 4) == "Cider") {
-            temp = ciders.length;
-            ciders[temp] = detailedBeers[i];
+            ciders.push(detailedBeers[i]);
         }
         else {
-            temp = otherBeverages.length;
-            otherBeverages[temp] = detailedBeers[i];
+            otherBeverages.push(detailedBeers[i]);
         }
     }
 
@@ -174,13 +173,12 @@ function sortBeverageByType () {
 
 }
 
-// inventory_append&beer_id=?&amount=?&price=?
-// Amount:
-
+// TODO
 // Takes a list of beer_id's and amount (2d structure, should be [[beer_id,amount],[beer_id,amount]..]), together with user
 // Since we can't send an amount, we have to send the HTTP Get for each beer. The amount of times this is done
 // is controlled by the second column in beerarray.
 // Choco: Is it necessary to define beerarray and user first?
+// Yes, it will be when we use the order to send it to the API. IN PROGRESS is important to point out.
 function addPurchase(beerarray, user) {
     for (var i = 0; i < beerarray.length; i++) {
         var beer_id = beerarray[i][0];
