0ab4ce71c6f4df2dbb3682c919e37c407ebf91c5
Quaryn <menacelol@hotmail.com>
2017-02-23 18:44:23 +0100

    transitioned to jquery instead

--- js/httpRequests.js
+++ js/httpRequests.js
@@ -4,21 +4,22 @@
 
 
 //This will make a HTTP GET request, where we request to get the info from URL
-//true-parameter makes it asynchronous
+//redid with jquery, use JSON.stringify to convert data to string.
+// not asynchronus, if someone wants to try to make it so, go ahead ;)
+
 function HTTPGetRequest(theAction) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://pub.jamaica-inn.net/fpdb/api.php?" + theAction, true);
-    xhr.send();
+    var jsonAPI = "http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod" + theAction;
 
-    //processRequest handler is called when readystate changes.
-    xhr.addEventListener("readystatechange", processRequest, false);
+    var jVar;
 
-    xhr.onreadystatechange = processRequest;
-
-    function processRequest(e) {
-        // rS == 4 & status == 200 => all is okay
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            return JSON.parse(xhr.responseText);
+    $.ajax({
+        url: jsonAPI,
+        dataType: 'json',
+        async: false,
+        success: function(json) {
+            jVar = json;
         }
-    }
+    });
+
+    return jVar;
 }
