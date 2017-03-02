793e65fe76a05d4f3aaab25bbc2d72668523326f
Hullberg <johan.hullberg@gmail.com>
2017-03-02 10:53:06 +0100

    kept working on translate

--- js/translate.js
+++ js/translate.js
@@ -9,20 +9,26 @@ function docLoaded(fn) {
     }
 }
 
-function translate(lang, string) {
-    // Lookup in the JSON-file what the word is in a certain language
+// All items that should be translated should belong to same class, and their key and value should exist in translations.json
+function translate(lang) {
+    // Find the class, change it's language attribute to the new language
+    document.getElementById("language").lang = lang;
+    els = document.getElementsByClassName("language");
 
+    // Lookup in the JSON-file what the word is in a certain language
     // json is the item we get from translations.json
     $.getJSON("translations.json", function( json ) {
 
         // the word in JSON we will look at, as for example one item will be
         // ['language']['sv']['Welcome','Welcome to the Flying Dutchman']
-        word = json['language'][lang][string];
-        // We want the value, after entering the key.
+        words = json['language'][lang]
+        // for each item in translations.json, we will look up the correct word.
+        $.each(words, function (key, value) {
+            console.log(json['language'][lang]['Welcome']);
+            console.log(key);
+            $("." + key).text(value);
+        });
 
-
-
-        // $("." + key).text(val);
     });
 
 
