4cc1b39f351357f907d1825027d13840e44c34c9
Quaryn <menacelol@hotmail.com>
2017-03-03 15:43:55 +0100

    sorting works

--- js/menu.js
+++ js/menu.js
@@ -12,40 +12,35 @@ function docLoaded(fn) {
 }
 
 
-function addToCart (item) {
-
-}
-
 
 function showInfo(beer_id) {
 
     var response = getDetails(beer_id);
 
+
     var output = "Namn: " + response[0]["namn"] + "<br>Pris: " + response[0]["prisinklmoms"] + "<br>Volym: " + response[0]["volymiml"] + "<br>Varugrupp: " + response[0]["varugrupp"] +
             "<br>Förpackning: " + response[0]["forpackning"] + "<br>Ursprung: " + response[0]["ursprunglandnamn"] + "<br>Producent: " +
         response[0]["producent"] + "<br>Alkoholhalt: " + response[0]["alkoholhalt"];
     document.getElementById("beerInfo").style.display = "initial";
+
     $('#beerInfo').html(output);
 
 }
 
-function displayItems(){
-    var joutput = document.getElementById("mainmenu");
-    var jsonResponse = getDrinks();
 
-    var cart = [];
-    var output = "";
+function displaySorted(sortedArray, place){
+
+    output = "";
+
+    for (var key in sortedArray) {
+
+        output += "<li>" + "<a class='clickInfo' href='#' data-name =" + sortedArray[key]["nr"] + ">" + sortedArray[key]["pname"] +
+            "<br>Price: " + sortedArray[key]["price"] +  "</a></li>";
+
+    }
 
 
-    for (var key in jsonResponse) {
-
-     output += "<li>" + "<a class='clickInfo' href='#' data-name =" + JSON.stringify(jsonResponse[key].beer_id) + ">" + JSON.stringify(jsonResponse[key].namn) +
-         "<br>Price: " + JSON.stringify(jsonResponse[key].pub_price) +
-         "<br>Stock: " + JSON.stringify(jsonResponse[key].count) + "</a></li>";
-
-     }
-
-    $('#mainmenu').html(output)
+   $(place).append(output);
 
     $(".clickInfo").click(function (event) {
         event.preventDefault();
@@ -53,10 +48,11 @@ function displayItems(){
         var id = $(this).attr("data-name");
         showInfo(id);
     })
+
 }
 
 function indexPageLoaded() {
-    displayItems();
+    sortBeverageByType();
     panels();
 }
 
